7e54c7c10f4ba7bffb5558cb9bd5b85c66f6b5d0
Diego Greco <greco.diego@gmail.com>
2022-09-25 19:12:39 -0300

    fix button disabled

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -7,11 +7,15 @@ export function validateField({
 	validateType,
 	value,
 }) {
-	function checkCondition(condition, name) {
+	function checkCondition({ condition, name }) {
+		console.log('hola')
+		console.log('name', name)
 		if (condition) {
+			console.log('true')
 			setInputValidState(true)
 			setFormValidState({ ...formValidState, [name]: true })
 		} else {
+			console.log('false')
 			setInputValidState(false)
 			setFormValidState({ ...formValidState, [name]: false })
 		}
@@ -19,32 +23,29 @@ export function validateField({
 
 	switch (validateType) {
 		case 'letters':
-			checkCondition(
-				value.length >= validateQuanty && /^[a-zA-Z]+$/.test(value),
-				validateType,
-				name
-			)
+			checkCondition({
+				condition: value.length >= validateQuanty && /^[a-zA-Z]+$/.test(value),
+				name: name,
+			})
 			break
 		case 'address':
-			checkCondition(
-				value.length >= validateQuanty && /^[0-9a-zA-Z]+$/.test(value),
-				validateType,
-				name
-			)
+			checkCondition({
+				condition:
+					value.length >= validateQuanty && /^[0-9a-zA-Z]+$/.test(value),
+				name: name,
+			})
 			break
 		case 'email':
-			checkCondition(
-				value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i),
-				validateType,
-				name
-			)
+			checkCondition({
+				condition: value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i),
+				name: name,
+			})
 			break
 		case 'numbers':
-			checkCondition(
-				value.length >= validateQuanty && /^[0-9]+$/.test(value),
-				validateType,
-				name
-			)
+			checkCondition({
+				condition: value.length >= validateQuanty && /^[0-9]+$/.test(value),
+				name: name,
+			})
 			break
 		case 'any':
 			setInputValidState(true)
